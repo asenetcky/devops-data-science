a0be5acd129ca5e82b9cfb8ae098f6e5521568b0
asenetcky <asenetcky@gmail.com>
2025-01-01 07:10:07 -0500

    update data folder location

--- build-docker-image.R
+++ build-docker-image.R
@@ -2,12 +2,12 @@ library(vetiver)
 library(pins)
 library(plumber)
 
-board <- board_folder(fs::path_wd("data/models/"))
+board <- board_folder("~/data/models/")
 
 vetiver_prepare_docker(
   board,
   name = "penguin_model",
-  version = "20250101T020503Z-99149",
+  version = "20250101T120810Z-99149",
   docker_args = list(
     port = 8080
   )
